35549f33a0bc03d83b7ee10f4b9582eef2ddc711
bellcross11 <thousandsuns11@gmail.com>
2023-03-01 15:05:06 +0800

    configure DAL

--- LoanCalculator/LoanCalculator.DataAccessLayer/Repository/GenericRepository.cs
+++ LoanCalculator/LoanCalculator.DataAccessLayer/Repository/GenericRepository.cs
@@ -1,12 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace LoanCalculator.DataAccessLayer.Repository
 {
-    public class GenericRepository
+    public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
     {
+        private readonly DbContext _context;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<TEntity> Search(Expression<Func<TEntity, bool>> filter = null, 
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, 
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>();
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public TEntity Get(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().RemoveRange(entities);
+        }
     }
 }
--- LoanCalculator/LoanCalculator.DataAccessLayer/Repository/IRepository.cs
+++ LoanCalculator/LoanCalculator.DataAccessLayer/Repository/IRepository.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace LoanCalculator.DataAccessLayer.Repository
 {
-    public interface IRepository
+    public interface IRepository<TEntity> where TEntity : class
     {
+        IEnumerable<TEntity> Search(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+        TEntity Get(int id);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
     }
 }
