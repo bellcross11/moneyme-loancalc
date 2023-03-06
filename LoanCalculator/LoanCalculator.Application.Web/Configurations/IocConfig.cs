8e651f83e185dc439ec1c19df13fc3ef695897a7
bellcross11 <thousandsuns11@gmail.com>
2023-03-06 19:11:10 +0800

    add IoC template, AutoFac and PMT ExcelFunctions

---
+++ LoanCalculator/LoanCalculator.Application.Web/Configurations/IocConfig.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using LoanCalculator.DataAccess.Core;
+using LoanCalculator.DataAccess.Core.IRepositories;
+using LoanCalculator.DataAccess.Persistence;
+using LoanCalculator.DataAccess.Persistence.EFContext;
+using LoanCalculator.DataAccess.Persistence.Repositories;
+using System.Data.Entity;
+using System.Reflection;
+using System.Web.Http;
+
+namespace LoanCalculator.Application.Web.Configurations
+{
+    public class IocConfig
+    {
+        public static IContainer Container;
+
+        // Call this method in GlobalAsax for dependency injection
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            //builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().SingleInstance();
+            //builder.RegisterType<LoanRepository>().As<ILoanRepository>().SingleInstance();
+            //builder.RegisterInstance<DbContext>(new LoanDBContext()).SingleInstance();
+
+            var container = builder.Build();
+
+            return container;
+        }
+    }
+}
--- LoanCalculator/LoanCalculator.Application.Web/Global.asax.cs
+++ LoanCalculator/LoanCalculator.Application.Web/Global.asax.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using LoanCalculator.Application.Web.Configurations;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -13,6 +10,9 @@ namespace LoanCalculator.Application.Web
     {
         protected void Application_Start()
         {
+            // AutoFac
+            IocConfig.Initialize(GlobalConfiguration.Configuration);
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
